59cff123c0720ced687a555c95df9441a323549e
Chan Li Liao <cliao119@gmail.com>
2020-12-08 22:52:18 -0500

    finish connecting to mongoDB

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-const connnectDB = async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
+      useUnifiedTopology: true,
       useFindAndModify: false,
     });
 
