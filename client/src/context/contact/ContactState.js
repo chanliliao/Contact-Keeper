787d2839ebbae41fe2c531edef0b38fcfd957db8
Chan Li Liao <cliao119@gmail.com>
2020-12-12 20:59:58 -0500

    start comtext

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,5 @@
+import React, { useReduder } from 'react';
+import uuid from 'uuid';
+import ContactContext from '../contact/contactContext';
+import contactReducer from './contactReducer';
+import {} from '';
---
+++ client/src/context/contact/contactContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const ccontactContext = createContext();
+
+export default contactContext;
