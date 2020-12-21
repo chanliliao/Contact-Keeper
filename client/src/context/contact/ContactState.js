7e22a12bd6137b755dea20c37c73ee19f980774d
Chan Li Liao <cliao119@gmail.com>
2020-12-21 18:19:12 -0500

    fixed unused var

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -70,7 +70,7 @@ const ContactState = (props) => {
   //delete contact
   const deleteContact = async (id) => {
     try {
-      const res = await axios.delete(`/api/contacts/${id}`);
+      await axios.delete(`/api/contacts/${id}`);
       dispatch({ type: DELETE_CONTACT, payload: id });
     } catch (err) {
       dispatch({ type: CONTACT_ERROR, payload: err.response.msg });
